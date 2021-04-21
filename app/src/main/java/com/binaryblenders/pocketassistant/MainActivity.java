02d15362824c705d12976273251453ad0f3aecb2
Kumaran <kumarans1508@gmail.com>
2021-04-21 13:47:55 +0530

    home page

--- app/src/main/java/com/binaryblenders/pocketassistant/MainActivity.java
+++ app/src/main/java/com/binaryblenders/pocketassistant/MainActivity.java
@@ -1,14 +1,22 @@
 package com.binaryblenders.pocketassistant;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
     }
 }
